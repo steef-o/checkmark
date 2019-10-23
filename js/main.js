478a9373da3240a56fb37d09176ff6901b9a23a8
Steffen Andre Hagen <steffen.hagen@knowit.no>
2019-10-23 10:32:37 +0200

    styling changes, added custom color dependent on state

--- js/main.js
+++ js/main.js
@@ -12,20 +12,33 @@ const loader = lottie.loadAnimation({
     path: 'data/v06.json' // the path to the animation json
 });
 
-loader.addEventListener('data_ready', function() {
+loader.addEventListener('DOMLoaded', function() {
+    const htmlCollection = document.getElementsByClassName('checkmark-color');
     loader.playSegments([1, 34], true);
 
+    // Success
     success.addEventListener('click', function() {
         loader.addEventListener('loopComplete', function() {
             loader.playSegments([35, 120], true);
             loader.loop = false;
+
+            for (let i = 0; i < htmlCollection.length; i++) {
+                htmlCollection[i].classList.add('success');
+                htmlCollection[i].classList.remove('error');
+            }
         });
     });
 
+    // Error
     error.addEventListener('click', function() {
         loader.addEventListener('loopComplete', function() {
             loader.playSegments([121, 200], true);
             loader.loop = false;
+
+            for (let i = 0; i < htmlCollection.length; i++) {
+                htmlCollection[i].classList.add('error');
+                htmlCollection[i].classList.remove('success');
+            }
         });
     });
 });
